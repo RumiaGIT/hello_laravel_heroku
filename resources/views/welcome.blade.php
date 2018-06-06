7d16b034fb698649a19c727a98bc8e406f0b1d70
ronnievm <ronnievm20@outlook.com>
2018-06-06 22:19:49 +0200

    Automatic deployment test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 	    <div class="panel-heading">Welcome</div>
 
 	    <div class="panel-body">
-	        Your Application's Landing Page. TEST TEST
+	        Your Application's Landing Page. TEST TEST SUPERTEST
 	    </div>
 	</div>
 </div>
