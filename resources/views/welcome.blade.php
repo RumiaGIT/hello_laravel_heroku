860d37322093586c6016dbcc40e9fe35ef072895
ronnievm <ronnievm20@outlook.com>
2018-06-06 22:14:22 +0200

    GitHub test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 	    <div class="panel-heading">Welcome</div>
 
 	    <div class="panel-body">
-	        Your Application's Landing Page.
+	        Your Application's Landing Page. TEST
 	    </div>
 	</div>
 </div>
